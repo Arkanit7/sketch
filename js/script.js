a7a9ba99b5e9a60a4157fdf8b8a9933a726b5f6d
Arkanit7 <61657673+Arkanit7@users.noreply.github.com>
2020-06-02 00:29:01 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 	var icon = {
-		url: '../img/map.svg',
+		url: 'img/map.svg',
 		scaledSize: new google.maps.Size(24, 29),
 		anchor: new google.maps.Point(9, 10)
 	}
